93a44b0fbf9ffaa645709a32468759cd5c64f63d
Kevin N <63832406+kevin-nyquist@users.noreply.github.com>
2023-11-06 15:36:24 -0500

    Added way to generate pandas dataframe with audio attributes from an album

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -1,8 +1,6 @@
 # Following Spotipy tutorial at https://github.com/spotipy-dev/spotipy/blob/master/TUTORIAL.md
 
 import spotipy
-import pandas as pd
-import json
 from spotipy.oauth2 import SpotifyOAuth
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="1e247c491dfe432680d7203916b4b20a",
@@ -12,22 +10,9 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="1e247c491dfe432680d720
 
 def get_playlist() :
     top_200_id = "7dPdaFrplLZD90rvfG8ZuB"
-    playlist = sp.playlist(top_200_id, fields="tracks.items(track(name,id))", market="ES", additional_types=('track', ))
-    items = playlist['tracks']['items']
-    df = pd.DataFrame(items)
-    df['track_id'] = df['track'].apply(lambda x: x['id'])
-    df['track_name'] = df['track'].apply(lambda x: x['name'])
-    df = df[['track_id', 'track_name']]
-    return df
-
-def get_track_audio_attributes(df: pd.DataFrame) :
-    track_ids = df['track_id']
-    tracks = sp.audio_features(track_ids)
-    df = pd.concat([df, pd.DataFrame(tracks)], axis=1)
-    print(df)
+    return sp.playlist(top_200_id, fields="tracks.items(track(name,id))", market="ES", additional_types=('track', ))
 
 def main() :
-    """
     taylor_uri = 'spotify:artist:06HL4z0CvFAxyc27GXpf02'
 
     results = sp.artist_albums(taylor_uri, album_type='album')
@@ -38,10 +23,9 @@ def main() :
 
     for album in albums:
         print(album['name'])
-    """
-    df = get_playlist()
-    df = get_track_audio_attributes(df)
-    print(df)
+    
+    playlist = get_playlist()
+    print(playlist)
 
 if __name__ == '__main__':
     main()

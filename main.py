8ad7771e986b54e6cdd5c4a04bd8c82486027a49
Kevin N <63832406+kevin-nyquist@users.noreply.github.com>
2023-11-07 11:43:39 -0500

    Created Nov7_audio_attributes dataset - needs cleaning

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import json
 from spotipy.oauth2 import SpotifyOAuth
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="1e247c491dfe432680d7203916b4b20a",
-                                               client_secret="",
+                                               client_secret="1e14d1db06d54f0cb6122aac6902b273",
                                                redirect_uri="http://localhost:1234",
                                                scope="user-library-read"))
 
@@ -19,7 +19,8 @@ def get_playlist() :
     df['track_name'] = df['track'].apply(lambda x: x['name'])
     df['album_name'] = df['track'].apply(lambda x: x['album']['name'])
     df['album_id'] = df['track'].apply(lambda x: x['album']['id'])
-    df = df[['track_id', 'track_name', 'album_name', 'album_id']]
+    df['popular'] = True
+    df = df[['track_id', 'track_name', 'album_name', 'album_id', 'popular']]
     return df
     
 def get_track_audio_attributes(df: pd.DataFrame) :
@@ -31,36 +32,31 @@ def get_track_audio_attributes(df: pd.DataFrame) :
 def get_5_songs(df: pd.DataFrame) :
     album_df = df['album_id']
     album_names = df['album_name']
+    track_names = df['track_name']
     
-    #print(album_ids)
+    #print(track_names)
     index = 0
     for album in album_df:
         albums = sp.album_tracks(album, limit=5, offset=0, market="ES")
         for i in range(5):
-            new_dict = {
-            'track_id': albums['items'][0]['id'],
-            'track_name': albums['items'][0]['name'],
-            'album_name': album_names[index],
-            'album_id': album
-            }
-            new_df = pd.DataFrame(new_dict, index=[0])
-            df = pd.concat([df, new_df], ignore_index=True)
+            try :
+                if (albums['items'][i]['name'] != track_names[index]) :
+                    new_dict = {
+                    'track_id': albums['items'][i]['id'],
+                    'track_name': albums['items'][i]['name'],
+                    'album_name': album_names[index],
+                    'album_id': album,
+                    'popular': False
+                    }
+                    new_df = pd.DataFrame(new_dict, index=[0])
+                    df = pd.concat([df, new_df], ignore_index=True)
+            except IndexError as e :
+                    break
         index+=1
-        if (index == 10) :
-            break
-        
     return df
-    #return pd.concat([df, item_names], ignore_index=True)
-    """
-    for track in album_ids:
-        other_song = sp.album_tracks(track, limit=5, offset=0, market="ES")
-        df.append(other_song)
-    #df = pd.concat([df, pd.DataFrame(other_song)], axis=0)
-    return df
-    """
     
 def get_track_audio_attributes(df: pd.DataFrame) :
-    track_ids = df['track_id'] #[101:200] doesn't go in the correct rows
+    track_ids = df['track_id'] #preformed [1:100], [100:200], [200:300], [300:400], and [400:]
     tracks = sp.audio_features(track_ids)
     df = pd.concat([df, pd.DataFrame(tracks)], axis=1)
     return df
